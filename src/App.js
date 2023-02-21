ce1fdf8f4437a4d0bebad8cb66ec3f8170ed309d
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-02-21 19:08:43 +0200

    kanban

--- src/App.js
+++ src/App.js
@@ -3,43 +3,118 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export default function App() {
   const finalSpaceCharacters = [
-    { id: "1", name: "one" },
-    { id: "2", name: "two" },
-    { id: "3", name: "three" },
+    { id: "1", name: "Sleeping" },
+    { id: "2", name: "Running" },
+    { id: "3", name: "Eating" },
+    { id: "4", name: "Playing" },
+
   ];
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
-  function handleOnDragEnd(result) {
+
+  const taskStatus = {
+    requested: {
+      name: "Requested",
+      items: finalSpaceCharacters
+    },
+    toDo: {
+      name: "To do",
+      items: []
+    },
+    inProgress: {
+      name: "In Progress",
+      items: []
+    },
+    done: {
+      name: "Done",
+      items: []
+    }
+  };
+
+
+  
+
+
+
+  function onDragEnd(result, columns, setColumns) {
     console.log(result);
     if (!result.destination) return;
+    const { source, destination } = result;
 
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
 
-    updateCharacters(items);
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+
+    
+    // const items = Array.from(characters);
+    // const [reorderedItem] = items.splice(result.source.index, 1);
+    // items.splice(result.destination.index, 0, reorderedItem);
+    // updateCharacters(items);
   }
+  const [columns, setColumns] = useState(taskStatus);
 
   return (
     <>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="characters">
+    <div className='page-container'>
+      <DragDropContext  onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+      {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              
+              <div
+              
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center"
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }} >
+        <Droppable droppableId={columnId} key={columnId}>
           {(provided) => (
             <ul
               className="characters"
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
-              {characters.map(({ id, name }, index) => {
+              {column.items.map((item, index) => {
                 return (
-                  <Draggable key={id} draggableId={id} index={index}>
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
                     {(provided) => (
                       <li
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                       >
-                        {name}
-                      </li>
+                      {item.name}                      </li>
                     )}
                   </Draggable>
                 );
@@ -48,7 +123,21 @@ export default function App() {
             </ul>
           )}
         </Droppable>
+      
+             
+        </div>
+              </div>
+              
+            );
+          })}
       </DragDropContext>
+      </div>
+        
+        
+     
     </>
   );
 }
+
+
+
