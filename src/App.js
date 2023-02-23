38eb310086b71bb0ef89ff5a71f6d4d83aee9823
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-02-23 18:33:02 +0200

    kanban

--- src/App.js
+++ src/App.js
@@ -1,86 +1,95 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { AiFillDelete } from "react-icons/ai";
 export default function App() {
-  const [task,setTask]=useState('');
-  const [taskItems,setTaskItems]=useState([])
-  const [taskStatus,setTaskStatus]=useState({requested: {
-    name: "Requested",
-    items: []
-  },
-  toDo: {
-    name: "To do",
-    items: []
-  },
-  inProgress: {
-    name: "In Progress",
-    items: []
-  },
-  done: {
-    name: "Done",
-    items: []
-  }})
+  const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([]);
+  const [shoeDeleteIcon, setShoeDeleteIcon] = useState(true);
+  const [taskStatus, setTaskStatus] = useState({
+    requested: {
+      name: "Requested",
+      items: [],
+    },
+    toDo: {
+      name: "To do",
+      items: [],
+    },
+    inProgress: {
+      name: "In Progress",
+      items: [],
+    },
+    done: {
+      name: "Done",
+      items: [],
+    },
+  });
   const [columns, setColumns] = useState(taskStatus);
-
 
   const getTaskStatusFromLocalStorage = () => {
     let info = localStorage.getItem("droppable");
     if (info === null) {
-     
       return taskStatus;
-    }else
-     
-
-    return JSON.parse(info);
+    } else return JSON.parse(info);
   };
 
-  
-   //Add New taskItem
+  //Add New taskItem
 
-   const addNewTaskItem = () => {
-    if (task.trim() == null ) return;
-    let id=uuidv4()
-    
+  const addNewTaskItem = () => {
+    if (task.trim() == null) return;
+    let id = uuidv4();
+
     const newTaskItem = {
-      id:id,
+      id: id,
       title: task,
-      
     };
-    const tempTaskItems=[...taskItems]
+    const tempTaskItems = [...taskItems];
     tempTaskItems.push(newTaskItem);
-    // console.log(taskItems)
-    
-    // setTaskItemsToLocalStorage();
 
-    const tempStatus={...taskStatus}
-    tempStatus.requested.items=[...tempTaskItems];
-    setTaskItems(tempTaskItems)
+    
+
+    const tempStatus = { ...taskStatus };
+    tempStatus.requested.items = [...tempTaskItems];
+    setTaskItems(tempTaskItems);
     setTaskStatus(tempStatus);
-    setColumns(tempStatus)
-    localStorage.setItem('droppable',JSON.stringify(tempStatus))
-    console.log(tempStatus)
+    setColumns(tempStatus);
+    localStorage.setItem("droppable", JSON.stringify(tempStatus));
+    console.log(tempStatus);
 
-    setTask('');
-    
+    setTask("");
   };
+
+  //RemoveTask
+  const removeTask = (taskItemRemoved, e) => {
+    e.stopPropagation();
+    const tempStatus = { ...taskStatus };
+    {
+      Object.entries(tempStatus).map(([columnId, column], index) => {
+        column.items.map((item, index) => {
+          if (item.id === taskItemRemoved.id) {
+            column.items.splice(index, 1);
+          }
+        });
+      });
+    }
+    setTaskStatus(tempStatus);
+    console.log(tempStatus);
+    localStorage.setItem("droppable", JSON.stringify(tempStatus));
+  };
+
   // localStorage.clear()
   useEffect(() => {
-    
-    let tempStatus=getTaskStatusFromLocalStorage();
-    setTaskItems(tempStatus.requested.items)
+    let tempStatus = getTaskStatusFromLocalStorage();
+    setTaskItems(tempStatus.requested.items);
     setTaskStatus(tempStatus);
-    setColumns(tempStatus)
-    console.log(tempStatus)
-    
+    setColumns(tempStatus);
+    console.log(tempStatus);
   }, []);
- 
-
 
   function onDragEnd(result, columns, setColumns) {
     // console.log(result);
     if (!result.destination) return;
     const { source, destination } = result;
-
 
     if (source.droppableId !== destination.droppableId) {
       const sourceColumn = columns[source.droppableId];
@@ -89,106 +98,118 @@ export default function App() {
       const destItems = [...destColumn.items];
       const [removed] = sourceItems.splice(source.index, 1);
       destItems.splice(destination.index, 0, removed);
-      const updateColumnsObjectAfterDrag=
-      {
+      const updateColumnsObjectAfterDrag = {
         ...columns,
         [source.droppableId]: {
           ...sourceColumn,
-          items: sourceItems
+          items: sourceItems,
         },
         [destination.droppableId]: {
           ...destColumn,
-          items: destItems
-        }
-      }
-      localStorage.setItem('droppable',JSON.stringify(updateColumnsObjectAfterDrag))
-      console.log(updateColumnsObjectAfterDrag)
+          items: destItems,
+        },
+      };
+      localStorage.setItem(
+        "droppable",
+        JSON.stringify(updateColumnsObjectAfterDrag)
+      );
+      // console.log(updateColumnsObjectAfterDrag);
       setColumns(updateColumnsObjectAfterDrag);
-     
     } else {
       const column = columns[source.droppableId];
       const copiedItems = [...column.items];
       const [removed] = copiedItems.splice(source.index, 1);
       copiedItems.splice(destination.index, 0, removed);
-      const updateColumnsObjectAfterDrag={
+      const updateColumnsObjectAfterDrag = {
         ...columns,
         [source.droppableId]: {
           ...column,
-          items: copiedItems
-        }
-      }
-      localStorage.setItem('droppable',JSON.stringify(updateColumnsObjectAfterDrag))
-      console.log(updateColumnsObjectAfterDrag)
+          items: copiedItems,
+        },
+      };
+      localStorage.setItem(
+        "droppable",
+        JSON.stringify(updateColumnsObjectAfterDrag)
+      );
+      // console.log(updateColumnsObjectAfterDrag);
       setColumns(updateColumnsObjectAfterDrag);
-      
     }
   }
 
- 
   return (
     <>
-    <div className='page-container'>
-      <div className='input-container'>
-          <input type='text' placeholder="Add Task" value={task} onChange={(e)=>setTask(e.target.value)} />
+      <div className="page-container">
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Add Task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
           <button onClick={addNewTaskItem}>+ADD</button>
-          </div>
-    <div className='tasks-container'>
-      <DragDropContext  onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
-      {Object.entries(columns).map(([columnId, column], index) => {
-            return (
-              
-              <div
-              
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center"
-                }}
-                key={columnId}
-              >
-                <h2>{column.name}</h2>
-                <div style={{ margin: 8 }} >
-        <Droppable droppableId={columnId} key={columnId} > 
-          {(provided) => (
-            <ul
-              className="characters"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {column.items.map((item, index) => {
-                return (
-                  <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
-                    {(provided) => (
-                      <li
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                      
-                      {item.title}                      </li>
-                    )}
-                  </Draggable>
-                );
-              })}
-                {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      
-             
         </div>
-              </div>
-              
-            );
-          })}
-      </DragDropContext>
+        <div className="tasks-container">
+          <DragDropContext
+            onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+          >
+            {Object.entries(columns).map(([columnId, column], index) => {
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                  key={columnId}
+                >
+                  <h2>{column.name}</h2>
+                  <div style={{ margin: 8 }}>
+                    <Droppable droppableId={columnId} key={columnId}>
+                      {(provided) => (
+                        <ul
+                          className="characters"
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id.toString()}
+                                index={index}
+                              >
+                                {(provided) => (
+                                  <li
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <div className="delete-container">
+                                      <p> {item.title} </p>
+                                      {shoeDeleteIcon && (
+                                        <AiFillDelete
+                                          style={{ color: "red" }}
+                                          size={22}
+                                          onClick={(e) => removeTask(item, e)}
+                                        />
+                                      )}
+                                    </div>
+                                  </li>
+                                )}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </ul>
+                      )}
+                    </Droppable>
+                  </div>
+                </div>
+              );
+            })}
+          </DragDropContext>
+        </div>
       </div>
-        
-        
-     </div>
     </>
   );
 }
-
-
-
